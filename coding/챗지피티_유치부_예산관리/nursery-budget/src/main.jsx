a4bf07c27afbdf05392b955f283d7b9f0148de0a
kimjonghyeok62 <kimjonghyeok62@gmail.com>
2025-12-13 19:49:07 +0900

    build: Force emptyOutDir and add runtime version logger

--- coding/챗지피티_유치부_예산관리/nursery-budget/src/main.jsx
+++ coding/챗지피티_유치부_예산관리/nursery-budget/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import BudgetMain from './BudgetMain.jsx'
 import './index.css'
 
+console.log("%c VERSION: 2026 FINAL ", "background: #222; color: #bada55; font-size: 20px");
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BudgetMain />
